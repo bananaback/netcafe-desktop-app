45f69a62f494981f6bdcf31ba5b5331b828173fd
bananaback <58079308+bananaback@users.noreply.github.com>
2024-03-20 16:20:04 +0700

    Create AppConfig for storing application configuration constants.

---
+++ src/main/java/dev/hideftbanana/netcafejavafxapp/AppConfig.java
@@ -0,0 +1,6 @@
+package dev.hideftbanana.netcafejavafxapp;
+
+public class AppConfig {
+    public static final double LOGIN_STAGE_WIDTH = 875;
+    public static final double LOGIN_STAGE_HEIGHT = 570;
+}
