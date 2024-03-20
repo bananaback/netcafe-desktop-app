a66ecf2f4f579012a52b23e4fe397402b7aa732a
bananaback <58079308+bananaback@users.noreply.github.com>
2024-03-20 16:18:57 +0700

    Add scene manager and use it in App

--- src/main/java/dev/hideftbanana/netcafejavafxapp/App.java
+++ src/main/java/dev/hideftbanana/netcafejavafxapp/App.java
@@ -1,10 +1,8 @@
 package dev.hideftbanana.netcafejavafxapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,24 +11,14 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("admin_login_page"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(
-                App.class.getResource("/dev/hideftbanana/netcafejavafxapp/fxml/" + fxml + ".fxml"));
-        return fxmlLoader.load();
+        stage.setWidth(AppConfig.LOGIN_STAGE_WIDTH);
+        stage.setHeight(AppConfig.LOGIN_STAGE_HEIGHT);
+        stage.initStyle(StageStyle.UNDECORATED);
+        SceneManager sceneManager = new SceneManager(stage);
+        sceneManager.switchingScene("/dev/hideftbanana/netcafejavafxapp/fxml/admin_login_page.fxml");
+        sceneManager.showRootStage();
     }
 
     public static void main(String[] args) {
