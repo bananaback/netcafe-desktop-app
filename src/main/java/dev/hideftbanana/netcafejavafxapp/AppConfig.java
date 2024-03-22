3504fe560905a46be2a302183664e2d82ab6ccf7
bananaback <58079308+bananaback@users.noreply.github.com>
2024-03-22 10:09:17 +0700

    Define main stage size constants.

--- src/main/java/dev/hideftbanana/netcafejavafxapp/AppConfig.java
+++ src/main/java/dev/hideftbanana/netcafejavafxapp/AppConfig.java
@@ -3,4 +3,6 @@ package dev.hideftbanana.netcafejavafxapp;
 public class AppConfig {
     public static final double LOGIN_STAGE_WIDTH = 875;
     public static final double LOGIN_STAGE_HEIGHT = 570;
+    public static final double MAIN_STAGE_WIDTH = 1440;
+    public static final double MAIN_STAGE_HEIGHT = 1024;
 }
